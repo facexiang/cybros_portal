5992698d4dc0084dfe261b2c94efa77c5e6ae7e0
Eric-Guo <eric.guocz@gmail.com>
2019-10-30 10:05:33 +0800

    Change begin and end operation record logic.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_065625) do
+ActiveRecord::Schema.define(version: 2019_10_30_013257) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_065625) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "begin_operation"
+    t.string "end_operation"
     t.index ["user_id"], name: "index_cad_sessions_on_user_id"
   end
 
