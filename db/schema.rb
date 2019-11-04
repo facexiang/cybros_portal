3c1ac8401b5283dd2a3c9875f480bf0f8e9740be
Eric-Guo <eric.guocz@gmail.com>
2019-11-04 10:19:32 +0800

    New columns to store report view all access.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_013257) do
+ActiveRecord::Schema.define(version: 2019_11_04_021833) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.string "name", null: false
@@ -189,6 +189,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_013257) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "cad_session"
+    t.boolean "report_view_all"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
